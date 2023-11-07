d95b4486177b1c1a503442de081ef678b7deea80
alexanderjalexander <alexj18082002@gmail.com>
2023-11-07 00:55:24 -0500

    Started Carousel for Opportunities Section on Main Page

---
+++ src/components/OpportunityCarousel/opportunitycarousel.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+
+interface OpportunityCardProps {
+    image: string,
+    name: string,
+    desc: string,
+    time: string,
+    date: string,
+    location: string
+}
+
+function OpportunityCard({image, name, desc, time, date, location}: OpportunityCardProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
