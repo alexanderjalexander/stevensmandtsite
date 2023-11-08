a58b9e634b2b04d9834e3227d7542cc8923ce69a
alexanderjalexander <alexj18082002@gmail.com>
2023-11-07 21:34:06 -0500

    Remove Studio Session

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 export function Footer() {
     return (
         <div className={styles.footer_base}>
-            <Link className={styles.footer_link} href="/">BOOK A STUDIO SESSION</Link>
             <Link className={styles.footer_link} href="/">CONTACT</Link>
             <Link className={styles.footer_link} href="/">INSTAGRAM</Link>
             <Link className={styles.footer_link} href="/">YOUTUBE</Link>
