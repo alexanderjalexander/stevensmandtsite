6a0f5d74c97edefeaf7849a28a717244390ea71b
alexanderjalexander <alexj18082002@gmail.com>
2023-11-20 17:00:11 -0500

    Attempting to make a new carousel, need to fix vertical "stacking" issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,21 @@
-const events = [
-    { image: 'https://www.cnet.com/a/img/resize/20d6844768bd3f5f0df41deee97897423bcaf3c5/hub/2021/11/03/3c2a7d79-770e-4cfa-9847-66b3901fb5d7/c09.jpg?auto=webp&fit=crop&height=1200&width=1200',
-        name: 'HYPERPOP NIGHT',
-        desc: "Feat 622CHAOS, COLEMAN.X and SOPHIE SHREDZ",
-        time: "7:45 PM",
-        date: "9.23.2023",
-        location: "Babbio Atrium"
-    }
-];
+import {OpportunityCardsCarousel} from "@/components/OpportunityCarousel/opportunitycarousel";
 
 export default function Page() {
-  return (
-    <main>
-      <div>
-        <h1>Upcoming Events</h1>
+    return (
+        <main>
+            <div>
+                <h1>Upcoming Events</h1>
+                <OpportunityCardsCarousel />
+            </div>
 
-      </div>
+            <div>
+                <h1>Opportunities</h1>
+            </div>
 
-      <div>
-        <h1>Opportunities</h1>
-      </div>
-
-      <div>
-        <h1>New Releases</h1>
-        <p>Explore music created by the Music and Technology community at Stevens.</p>
-      </div>
-    </main>
-  )
+            <div>
+                <h1>New Releases</h1>
+                <p>Explore music created by the Music and Technology community at Stevens.</p>
+            </div>
+        </main>
+    )
 }
--- src/components/OpportunityCarousel/opportunitycarousel.tsx
+++ src/components/OpportunityCarousel/opportunitycarousel.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import {Carousel} from "@mantine/carousel";
+import {Paper, Text, Title, useMantineTheme, rem, Card, Group, Flex} from '@mantine/core';
+import classes from './opportunitycarousel.module.css';
+import {useMediaQuery} from "@mantine/hooks";
 
 interface OpportunityCardProps {
     image: string,
@@ -10,10 +14,107 @@ interface OpportunityCardProps {
     location: string
 }
 
+const data = [
+    {
+        image: 'https://www.cnet.com/a/img/resize/20d6844768bd3f5f0df41deee97897423bcaf3c5/hub/2021/11/03/3c2a7d79-770e-4cfa-9847-66b3901fb5d7/c09.jpg?auto=webp&fit=crop&height=1200&width=1200',
+        name: 'THING',
+        desc: "idk some desc",
+        time: "7:45 PM",
+        date: "9.23.2023",
+        location: "Babbio Atrium"
+    },
+    {
+        image: 'https://www.cnet.com/a/img/resize/20d6844768bd3f5f0df41deee97897423bcaf3c5/hub/2021/11/03/3c2a7d79-770e-4cfa-9847-66b3901fb5d7/c09.jpg?auto=webp&fit=crop&height=1200&width=1200',
+        name: 'Fall Open Mic',
+        desc: "Lorem Ipsum Lorem Ipsum",
+        time: "7:45 PM",
+        date: "9.30.2023",
+        location: "asdfasejfoiasjefo"
+    }
+]
+
 function OpportunityCard({image, name, desc, time, date, location}: OpportunityCardProps) {
     return (
-        <div>
+        <Card shadow="md" radius="lg" withBorder padding="xl">
+            <Card.Section>
+                <Paper
+                    p="xl"
+                    withBorder
+                    style={{ backgroundImage: `url(${image})` }}
+                    className={classes.card}>
 
-        </div>
+                    <Flex justify="center"
+                          align="center"
+                          direction="row"
+                          wrap="nowrap"
+                          mt="auto">
+                        <Text className={classes.category} size="md">
+                            {location}
+                        </Text>
+                        <Text className={classes.category} size="md">
+                            {time}
+                        </Text>
+                        <Text className={classes.category} size="md">
+                            {date}
+                        </Text>
+                    </Flex>
+
+                </Paper>
+            </Card.Section>
+            <Title order={3} className={classes.title}>
+                {name}
+            </Title>
+            <Text className={classes.category} size="xs">
+                {desc}
+            </Text>
+        </Card>
+        // <Paper
+        //     shadow="md"
+        //     p="xl"
+        //     radius="lg"
+        //     style={{ backgroundImage: `url(${image})` }}
+        //     className={classes.card}>
+        //     <div>
+        //         <Text className={classes.category} size="xs">
+        //             {time} | {date} | {location}
+        //         </Text>
+        //         <Title order={3} className={classes.title}>
+        //             {name}
+        //         </Title>
+        //         <Text className={classes.category} size="xs">
+        //             {desc}
+        //         </Text>
+        //     </div>
+        // </Paper>
+        // <Carousel.Slide>
+        //     <img src={image}/>
+        //     <h3>{name}</h3>
+        //     <h3>{time}</h3>
+        //     <h3>{date}</h3>
+        //     <h3>{location}</h3>
+        //     <p>{desc}</p>
+        // </Carousel.Slide>
     )
+}
+
+export function OpportunityCardsCarousel() {
+    const theme = useMantineTheme();
+    const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+    const slides = data.map((item) => (
+        <Carousel.Slide key={item.name}>
+            <OpportunityCard {...item} />
+        </Carousel.Slide>
+    ));
+
+    return (
+        <Carousel
+            slideSize={{base: '100%', sm: '50%'}}
+            slideGap={{base: rem(2), sm: 'xl'}}
+            align="start"
+            withIndicators
+            slidesToScroll={mobile ? 1 : 2}
+        >
+            {slides}
+        </Carousel>
+    );
 }
