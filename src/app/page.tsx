4f14859ef1b707cfab6a4431205e3dcefe442bc7
alexanderjalexander <alexj18082002@gmail.com>
2023-11-22 19:12:01 -0500

    Encase in div, max width loophole

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
 import {EventCardsCarousel, OpportunityCardsCarousel, ReleasesCardsCarousel} from "@/components/OpportunityCarousel/opportunitycarousel";
 import Header from "@/components/Header/header";
+import Image from "next/image"
 
 export default function Page() {
     return (
-        <main>
+        <div>
             <Header text="Music & Tech" />
-            <div>
-                <h1>Upcoming Events</h1>
-                <EventCardsCarousel />
-            </div>
+            <main>
+                <div>
+                    <h1>Upcoming Events</h1>
+                    <EventCardsCarousel />
+                </div>
 
-            <div>
-                <h1>Opportunities</h1>
-                <OpportunityCardsCarousel />
-            </div>
+                <div>
+                    <h1>Opportunities</h1>
+                    <OpportunityCardsCarousel />
+                </div>
 
-            <div>
-                <h1>New Releases</h1>
-                <p>Explore music created by the Music and Technology community at Stevens.</p>
-                <ReleasesCardsCarousel />
-            </div>
-        </main>
+                <div>
+                    <h1>New Releases</h1>
+                    <p>Explore music created by the Music and Technology community at Stevens.</p>
+                    <ReleasesCardsCarousel />
+                </div>
+            </main>
+        </div>
     )
 }
