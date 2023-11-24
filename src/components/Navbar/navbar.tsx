78f8db7bd2e769cbe04e8c7a1544f3bbb83f0e46
alexanderjalexander <alexj18082002@gmail.com>
2023-11-23 21:23:49 -0500

    Simple burger addition

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import Link from "next/link"
 import styles from "./navbar.module.css"
+import {Burger, Group} from "@mantine/core";
+import {useDisclosure} from "@mantine/hooks";
 
 const links = [
     { link: '/', label: 'Home' },
@@ -11,18 +15,28 @@ const links = [
 ];
 
 export function Navbar() {
+    const [opened, { toggle }] = useDisclosure(false);
+
     const items = links.map((link) => (
         <Link href={link.link} key={link.label} className={styles.links}> {link.label} </Link>
     ));
 
-    // TODO: Implement hamburger menu at some point with all the resources as well
+    // TODO: Implement hamburger menu
     return (
         <div className={styles.navbar}>
             <div className={styles.navbar_content}>
                 <Link  className={styles.home_link} href="/"><h2>M&T | SIT</h2></Link>
-                <div className={styles.links_container} hidden={false}>
+                <Group visibleFrom="sm" className={styles.links_container} hidden={false}>
                     {items}
-                </div>
+                </Group>
+                <Burger
+                    mt="auto"
+                    mb="auto"
+                    opened={opened}
+                    onClick={toggle}
+                    size="sm"
+                    hiddenFrom="sm"
+                />
             </div>
         </div>
     );
