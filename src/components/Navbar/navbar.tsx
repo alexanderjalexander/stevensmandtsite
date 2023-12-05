02fd5210cdf409e4fc22b98aebf0fbfd1187a044
alexanderjalexander <alexj18082002@gmail.com>
2023-12-04 23:35:19 -0500

    Hamburger done :D

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import styles from "./navbar.module.css"
-import {Burger, Drawer, Group} from "@mantine/core";
+import {Burger, Drawer, Flex, Group, UnstyledButton} from "@mantine/core";
 import {useDisclosure} from "@mantine/hooks";
 
 const links = [
@@ -17,12 +17,14 @@ const links = [
 export function Navbar() {
     const [menu_opened, { toggle:menu_toggle, close:close_menu }] = useDisclosure(false);
 
-
     const items = links.map((link) => (
         <Link href={link.link} key={link.label} className={styles.links}> {link.label} </Link>
     ));
 
-    // TODO: Implement hamburger menu
+    const menu_items = links.map((link) => (
+        <Link href={link.link} key={link.label} onClick={close_menu} className={styles.menu_links}> {link.label} </Link>
+    ));
+
     return (
         <div className={styles.navbar}>
             <div className={styles.navbar_content}>
@@ -46,12 +48,17 @@ export function Navbar() {
                     opened={menu_opened}
                     onClose={close_menu}
                     size="100%"
-                    padding="md"
+                    padding="xl"
                     c="#191528"
+                    hiddenFrom="sm"
                 >
-                {/*  TODO: FINISH THE BURGER MENU  */}
-
-
+                    <Flex
+                        gap="md"
+                        justify="flex-start"
+                        align="flex-start"
+                        direction="column">
+                        {menu_items}
+                    </Flex>
                 </Drawer>
             </div>
         </div>
