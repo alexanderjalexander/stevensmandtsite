4fbbab1b4e8828659c1a5498468ec938e8e69522
alexanderjalexander <alexj18082002@gmail.com>
2023-12-06 16:53:30 -0500

    Studio Booking starting implementation

--- src/app/studiobooking/page.tsx
+++ src/app/studiobooking/page.tsx
@@ -1,10 +1,18 @@
 import Header from "@/components/Header/header";
+import {Button} from "@mantine/core";
 
 export default function Page() {
     return (
         <div>
             <Header text="Studio Booking"/>
             <main>
+                <Button m="auto" component="a" href="https://tinyurl.com/K350BookingApplication">
+                    Book Here
+                </Button>
+                <p>Special Bookings are ONLY for professor-sanctioned ensembles (Ex. HMU 498), and Senior Thesis work. Questions about what activities are eligible for bookings can be emailed to <a href="mailto:shuashua@stevens.edu">shuashua@stevens.edu</a>. Other work (classwork, personal work, etc) can be done during open monitor hours.</p>
+                <p>Please apply at least 48 hours before the desired times. Bookings can only be made during monitor hours and not during any classes.</p>
+
+            {/* TODO: Add static lab hours assets */}
             </main>
         </div>
     )
