5ed66f1579a85375ffd024fac4cb15c5ec31ad1a
alexanderjalexander <alexj18082002@gmail.com>
2023-12-09 14:50:18 -0500

    Fixing apostrophe error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/Header/header";
 import Link from "next/link";
 import {Button} from "@mantine/core";
+import {headers} from "next/headers";
 
-export default function NotFound() {
+export default async function NotFound() {
     return (
         <div>
-            <Header text="Page Not Found"/>
+            <Header text="Not Found: "/>
             <main>
-                <h1>Whoops, that wasn't the right link.</h1>
+                <h1>Whoops, that wasn&apos;t the right link.</h1>
                 <p>Something happened between you going from here to there.</p>
                 <Button component={Link} href="/">Return Home</Button>
             </main>
