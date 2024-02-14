76df38b720513e2ccd12b061ac979ff8ff2a64a4
alexanderjalexander <alexj18082002@gmail.com>
2024-02-13 20:14:30 -0500

    Artists and Club Pages

---
+++ src/app/community/artists/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header/header";
+
+export default function Page() {
+    return (
+        // TODO: Need to implement Artists and Clubs pages as well
+        <div>
+            <Header text="Artists & Bands"/>
+            <main>
+            </main>
+        </div>
+    )
+}
