08b4e635d2d060e303b65f75b52dc31f26d9533a
alexanderjalexander <alexj18082002@gmail.com>
2024-02-13 20:15:35 -0500

    Removed TODOs

--- src/app/community/clubs/page.tsx
+++ src/app/community/clubs/page.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/Header/header";
 
 export default function Page() {
     return (
-        // TODO: Need to implement Artists and Clubs pages as well
         <div>
             <Header text="Clubs"/>
             <main>
